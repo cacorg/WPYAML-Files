a300f742d0c0274a05733ef53971dec45efc5feb
jcm2852 <jcm2582@unm.edu>
2018-08-08 15:23:30 -0600

    depricated resource logging

--- deploy.sh
+++ deploy.sh
@@ -39,8 +39,8 @@ az webapp config container set --subscription $ACI_SUBSCRIPTION --resource-group
 
 
 #links
-echo "https://portal.azure.com/#@cac.org/resource/subscriptions/a2ca24e0-461a-45f1-937c-531dd1fc24ee/resourceGroups/$ACI_APPNAME/providers/Microsoft.Web/sites/$ACI_APPNAME/appServices"
-echo "https://$ACI_APPNAME.azurewebsites.net"
+echo "Visit Resource --- https://portal.azure.com/#@cac.org/resource/subscriptions/a2ca24e0-461a-45f1-937c-531dd1fc24ee/resourceGroups/$ACI_APPNAME/providers/Microsoft.Web/sites/$ACI_APPNAME/appServices"
+echo "visit site --- https://$ACI_APPNAME.azurewebsites.net"
 
 
 #az group delete --subscription $ACI_SUBSCRIPTION --name $ACI_PERS_RESOURCE_GROUP --no-wait --verbose --yes
@@ -55,26 +55,26 @@ echo "https://$ACI_APPNAME.azurewebsites.net"
 # git clone: htdocs
 #cd ..
 #git clone https://github.com/WordPress/WordPress
-creds=($(az webapp deployment list-publishing-profiles --name $ACI_APPNAME --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP \
---query "[?contains(publishMethod, 'FTP')].[publishUrl,userName,userPWD]" --output tsv))
-curl -u ${creds[1]}:${creds[2]} $creds -Q "MKD database" -v
+#creds=($(az webapp deployment list-publishing-profiles --name $ACI_APPNAME --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP \
+#--query "[?contains(publishMethod, 'FTP')].[publishUrl,userName,userPWD]" --output tsv))
+#curl -u ${creds[1]}:${creds[2]} $creds -Q "MKD database" -v
 #wget --no-parent --recursive \
 #--no-directories --user=${creds[1]} --password=${creds[2]} ${creds[0]}
 
 
 
 # Create Persistent sQL DB
-az mysql server create --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --location "$ACI_PERS_LOCATION" --name $ACI_SQL  --location "$ACI_PERS_LOCATION" --admin-user $WORDPRESS_DB_NAME --admin-password $WORDPRESS_DB_PASSWORD --sku-name B_Gen4_1 --version 5.7
-az mysql server firewall-rule create --name ACI_FIREWALL --server $ACI_SQL --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
-az mysql db create --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --server-name $ACI_SQL --name $WORDPRESS_DB_NAME
-az webapp config appsettings set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --settings WORDPRESS_DB_HOST="$WORDPRESS_DB_HOST" WORDPRESS_DB_USER="$WORDPRESS_DB_USER" WORDPRESS_DB_PASSWORD="$WORDPRESS_DB_PASSWORD" WORDPRESS_DB_NAME="$WORDPRESS_DB_NAME" WP_REDIS_HOST="redis"
+#az mysql server create --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --location "$ACI_PERS_LOCATION" --name $ACI_SQL  --location "$ACI_PERS_LOCATION" --admin-user $WORDPRESS_DB_NAME --admin-password $WORDPRESS_DB_PASSWORD --sku-name B_Gen4_1 --version 5.7
+#az mysql server firewall-rule create --name ACI_FIREWALL --server $ACI_SQL --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+#az mysql db create --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --server-name $ACI_SQL --name $WORDPRESS_DB_NAME
+#az webapp config appsettings set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --settings WORDPRESS_DB_HOST="$WORDPRESS_DB_HOST" WORDPRESS_DB_USER="$WORDPRESS_DB_USER" WORDPRESS_DB_PASSWORD="$WORDPRESS_DB_PASSWORD" WORDPRESS_DB_NAME="$WORDPRESS_DB_NAME" WP_REDIS_HOST="redis"
 
 #UPDATE WEBAPP
-az webapp config container set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --multicontainer-config-type compose --multicontainer-config-file docker-compose-wordpress.yml
+#az webapp config container set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --multicontainer-config-type compose --multicontainer-config-file docker-compose-wordpress.yml
 
 #PERSISTENT STORAGE
 
-az webapp config appsettings set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
+#az webapp config appsettings set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
 
-az webapp config container set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --multicontainer-config-type compose --multicontainer-config-file docker-compose-wordpress.yml
-echo "https://portal.azure.com/#@cac.org/resource/subscriptions/a2ca24e0-461a-45f1-937c-531dd1fc24ee/resourceGroups/$ACI_APPNAME/providers/Microsoft.Web/sites/$ACI_APPNAME/appServices"
+#az webapp config container set --subscription $ACI_SUBSCRIPTION --resource-group $ACI_PERS_RESOURCE_GROUP --name $ACI_APPNAME --multicontainer-config-type compose --multicontainer-config-file docker-compose-wordpress.yml
+#echo "https://portal.azure.com/#@cac.org/resource/subscriptions/a2ca24e0-461a-45f1-937c-531dd1fc24ee/resourceGroups/$ACI_APPNAME/providers/Microsoft.Web/sites/$ACI_APPNAME/appServices"
